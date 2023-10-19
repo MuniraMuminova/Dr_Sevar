f53395eab8f621fccd6facec59dc09fa8f0fedf0
Munya <munirashovokhobova@gmail.com>
2023-10-19 15:47:25 +0500

    add name

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/Dr_Sevar/',
+  base: '/Dr_Sevar/',
   plugins: [vue()],
   resolve:{
     alias:{
